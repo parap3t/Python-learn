a16bbfe0e79b9c5504e5b47400b5efddae7a4976
parap3t <d.parkhomenko2005@gmail.com>
2024-11-25 17:10:51 +0500

    added last files in sixth lesson and answers for the tasks

--- Шестое занятие/string_oper.py
+++ Шестое занятие/string_oper.py
@@ -1,3 +1,4 @@
+# Основные операции для работы со строками
 str1 = "dsdso"
 str2 = "dsodks"
 
@@ -8,3 +9,8 @@ print(str1 > str2)
 print(str1 < str2)
 
 print(str1 == str2)
+
+print(str1 * 7)
+
+for _ in range(3):
+  print(str1, end="")
